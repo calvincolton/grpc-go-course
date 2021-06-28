12e7f9b00de3d96e1fe11358653f3176927a9abb
Calvin Colton <calvincolton@gmail.com>
2021-06-28 16:24:22 -0700

    greet client

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"google.golang.org/grpc"
+
+	"../greetpb"
+)
 
 func main() {
 	fmt.Println("Hello Client")
+	
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+
+	fmt.Printf("Created client: %f", c)
 }
